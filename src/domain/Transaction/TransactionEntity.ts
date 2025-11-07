72652b027876ddad50e045adce58e2f9e8e6037c
Pau Riquelme Catalina <pau.riquelme96@gmail.com>
2025-11-07 06:46:23 +0100

    Implement TransactionDomain

--- src/domain/Transaction/TransactionEntity.ts
+++ src/domain/Transaction/TransactionEntity.ts
@@ -3,9 +3,17 @@ import { TransactionModel } from "./TransactionModel";
 import { provide } from "@spoon-kit-legacy/providers/providers";
 import { TransactionApi } from "./TransactionApi";
 
+declare const toAccountEntity: any;
 export class TransactionEntity extends Entity<TransactionModel> {
   private api = provide(TransactionApi);
   model = new TransactionModel();
+
+  //account = toAccountEntity({ _id: this.model.account_id });
+  //account = calc(() => this.api.getAccount(this.model.account_id.get()));
+  async account() {
+    const result = await this.api.getAccount(this.model.account_id.get());
+    return toAccountEntity(result);
+  }
 
   save() {
     return this.commit(async (values) => {
--- src/domain/Transaction/TransactionModel.ts
+++ src/domain/Transaction/TransactionModel.ts
@@ -5,9 +5,13 @@ export type iTransaction = ModelValues<TransactionModel>;
 
 export class TransactionModel {
   _id = field<string>();
+  type = field<"income" | "expense" | "transfer" | "fractioned">();
   amount = field<number>();
   date = field<string>();
   description = field<string>();
   account_id = field<string>();
   forecast_id = field<string>();
+  //
+  transfer_account_id = field<string>();
+  child_transactions = field<iTransaction[]>();
 }
